bea482ba2d84b03648bb18777f1997a5318f59b7
GABRIEL YAGO RODRIGUES LEAL <gabriel.yrleal@senacsp.edu.br>
2025-02-11 22:16:36 -0300

    atual

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
  * @author Gabriel Yago
  */
 
+// ===================================================
+// Registro de service worker
+
+// Se o navegador de internet suportar este recurso 
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+        console.log("Service worker registrado!")
+    })
+}
+
+// ===================================================
+
+
 function calcular() {
     let gasolina = document.getElementById('gasolina').value
     let etanol = document.getElementById('etanol').value
